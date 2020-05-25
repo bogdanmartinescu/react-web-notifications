41954f779ab26423b32fccb8eca8e52f316c64a7
Bogdan Martinescu <bogdan.martinescu@hartmannindustries.com>
2020-05-25 22:54:02 +0300

    formatting, cleanup, package updates

--- src/index.js
+++ src/index.js
@@ -2,62 +2,61 @@ import { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class WebNotification extends Component {
+	state = {
+		notification: {},
+		options: {}
+	};
 
-  state = {
-    notification: {},
-    options: {}
-  }
+	componentDidMount() {
+		if (window.Notification) {
+			window.Notification.requestPermission((result) => {
+				if (result !== 'granted') return;
+				this.show();
+			});
+		}
+	}
 
-  componentDidMount() {
-    if(window.Notification) {
-      window.Notification.requestPermission( result => {
-          if(result !== 'granted') return;
-          this.show();
-      });
-    }
-  }
+	show() {
+		const { title, icon, body, timeout, onClickFn } = this.props;
 
-  show() {
-    const { title, icon, body, timeout, onClickFn } = this.props;
-    
-    if(!title) return;
+		if (!title) return;
 
-    if(icon) {
-        this.setState({ 
-            options: {...this.state.options, icon }
-        });
-    }
+		if (icon) {
+			this.setState({
+				options: { ...this.state.options, icon }
+			});
+		}
 
-    if(body) {
-        this.setState({
-            options: {...this.state.options, body }
-        });
-    }
+		if (body) {
+			this.setState({
+				options: { ...this.state.options, body }
+			});
+		}
 
-    this.setState({
-      notification: new window.Notification(title, this.state.options)
-    });
+		this.setState({
+			notification: new window.Notification(title, this.state.options)
+		});
 
-    if(onClickFn) {
-      this.state.notification.addEventListener("click", onClickFn, false);
-    }
+		if (onClickFn) {
+			this.state.notification.addEventListener('click', onClickFn, false);
+		}
 
-    window.setTimeout(() => {
-      this.state.notification.close();
-    }, (timeout || 5000));
-  }
+		window.setTimeout(() => {
+			this.state.notification.close();
+		}, timeout || 5000);
+	}
 
-  render () {
-    return null;
-  }
+	render() {
+		return null;
+	}
 }
 
 WebNotification.propTypes = {
-    title: PropTypes.string.isRequired,
-    icon: PropTypes.string,
-    body: PropTypes.string,
-    timeout: PropTypes.number,
-    onClickFn: PropTypes.func
+	title: PropTypes.string.isRequired,
+	icon: PropTypes.string,
+	body: PropTypes.string,
+	timeout: PropTypes.number,
+	onClickFn: PropTypes.func
 };
 
 export default WebNotification;
