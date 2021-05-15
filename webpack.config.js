12b4af38bc0594167899cf331193e2715ce26f4e
Bogdan Martinescu <bogdan.martinescu@hartmannindustries.com>
2021-05-15 14:33:15 +0300

    dependecies update

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,32 @@
-const webpack = require('webpack')
-const path = require('path')
+const webpack = require('webpack');
+const path = require('path');
 
 module.exports = {
-  entry: {
-    'react-web-notifications.min': './src/index.js'
-  },
-  externals: {
-    'react': {
-      root: 'React',
-      commonjs2: 'react',
-      commonjs: 'react',
-      amd: 'react'
-    }
-  },
-  module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loaders: 'babel-loader'
-    }]
-  },
-  output: {
-    filename: '[name].js',
-    path: path.join(__dirname, 'dist'),
-    publicPath: 'dist/',
-    library: 'ReactWebNotifications',
-    libraryTarget: 'umd'
-  }
-}
+	entry: {
+		'react-web-notifications.min': './src/index.js'
+	},
+	externals: {
+		react: {
+			root: 'React',
+			commonjs2: 'react',
+			commonjs: 'react',
+			amd: 'react'
+		}
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: ['babel-loader']
+			}
+		]
+	},
+	output: {
+		filename: '[name].js',
+		path: path.join(__dirname, 'dist'),
+		publicPath: 'dist/',
+		library: 'ReactWebNotifications',
+		libraryTarget: 'umd'
+	}
+};
